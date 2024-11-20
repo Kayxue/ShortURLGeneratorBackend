927d108cb4fc83555fdfec725c43f8d1b77bd47f
Kayxue <s0990006@gmail.com>
2024-11-20 18:43:59 +0800

    Try to publish to flyio

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import { Hono } from "jsr:@hono/hono";
 import { cors } from "jsr:@hono/hono/cors";
-import { initAuthConfig } from "npm:@hono/auth-js";
-import Credentials from "npm:@auth/core/providers/credentials";
 import { expandGlob } from "jsr:@std/fs";
 import IRouterExport from "./Interfaces/Interface.ts";
 
@@ -10,12 +8,12 @@ const hono = new Hono();
 hono.use(cors());
 
 hono.get("/", (c) => {
-	return c.text("Welcome to the ShortURL Backend");
+    return c.text("Welcome to the ShortURL Backend");
 });
 
 for await (const file of expandGlob(`${Deno.cwd()}/Router/**/*.ts`)) {
-	const { route, router }: IRouterExport = (await import(file.path)).default;
-	hono.route(route, router);
+    const { route, router }: IRouterExport = (await import(file.path)).default;
+    hono.route(route, router);
 }
 
-Deno.serve({ port: 3001 }, hono.fetch);
+Deno.serve({ port: 3000 }, hono.fetch);
