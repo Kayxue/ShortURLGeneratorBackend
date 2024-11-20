0adcc120ceead607d0dd5f1131eb83da2e18bc53
Kayxue <s0990006@gmail.com>
2024-11-20 23:25:36 +0800

    Test whether deno can read env

--- Router/RootRouter.ts
+++ Router/RootRouter.ts
@@ -1,9 +1,19 @@
 import { Hono } from "jsr:@hono/hono";
 import IRouterExport from "../Interfaces/Interface.ts";
+import { verify, hash } from "jsr:@felix/argon2";
 
 const router = new Hono();
 router.get("/:id", (c) => {
-	return c.text(c.req.param("id"));
+    return c.text(c.req.param("id"));
 });
+
+router.get("/hash/:pwd", async (c) => {
+    const { pwd } = c.req.param();
+    return c.text(await hash(pwd, { timeCost: 7, lanes: 7 }));
+});
+
+router.get("/env/env",c => {
+	return c.text(Deno.env.get("ENV"))
+})
 
 export default { route: "/", router } as IRouterExport;
