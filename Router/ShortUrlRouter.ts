033dcfce99a20c618e830bfaf291ddbfdc3e2f62
Kayxue <s0990006@gmail.com>
2024-11-23 15:02:57 +0800

    Short URL create route req body parsing

--- Router/ShortUrlRouter.ts
+++ Router/ShortUrlRouter.ts
@@ -3,13 +3,12 @@ import { nanoid } from "npm:nanoid";
 import { zValidator } from "npm:@hono/zod-validator";
 import IRouterExport from "../Interfaces/Interface.ts";
 import { createUrlSchema } from "../Schema/Schema.ts";
-import { z } from "npm:zod";
 
 const hono = new Hono();
 
 hono.post("/create", zValidator("json", createUrlSchema), async (c) => {
-	const data = await c.req.json();
-	return c.json(data);
+	const { userId, customParam, url, password } = await c.req.json();
+	return c.json({ userId, customParam, url, password });
 });
 
 export default { route: "/shorturl", router: hono } as IRouterExport;
--- Schema/Schema.ts
+++ Schema/Schema.ts
@@ -3,6 +3,6 @@ import { z } from "npm:zod";
 export const createUrlSchema = z.object({
 	userId: z.string().optional(),
 	customParam: z.string().optional(),
-	url:z.string(),
+	url: z.string(),
 	password: z.string().optional(),
 });
