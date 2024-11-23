cf0e97aa1cee4eff2b3c7d65ce4634455effea24
Kayxue <s0990006@gmail.com>
2024-11-23 15:32:56 +0800

    Created short url routes

--- Router/ShortUrlRouter.ts
+++ Router/ShortUrlRouter.ts
@@ -7,8 +7,24 @@ import { createUrlSchema } from "../Schema/Schema.ts";
 const hono = new Hono();
 
 hono.post("/create", zValidator("json", createUrlSchema), async (c) => {
-	const { userId, customParam, url, password } = await c.req.json();
-	return c.json({ userId, customParam, url, password });
+	const { userId, param, url, password } = await c.req.json();
+	//TODO: generate data and push into database
+	return c.json({ userId, param, url, password });
 });
+
+hono.patch("/:param",/* Login Middleware, Url Owned Middleware, zodValidator, */c => {
+	//TODO: Check the short url information exist, if exist, update the information.
+	return c.text("Update Path")
+})
+
+hono.get("/:param",c => {
+	//TODO: Get url to redirect to from database and check whether the short url need password to redirect
+	return c.text("url redirect")
+})
+
+hono.post("/:param/password",c => {
+	//TODO: Check password for the short url which need password verification.
+	return c.text("url password verification")
+})
 
 export default { route: "/shorturl", router: hono } as IRouterExport;
--- Schema/Schema.ts
+++ Schema/Schema.ts
@@ -2,7 +2,7 @@ import { z } from "npm:zod";
 
 export const createUrlSchema = z.object({
 	userId: z.string().optional(),
-	customParam: z.string().optional(),
+	param: z.string().optional(),
 	url: z.string(),
 	password: z.string().optional(),
 });
