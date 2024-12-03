4325e62f0d38a277948ec3d0545fcc0d37240d31
Kayxue <s0990006@gmail.com>
2024-12-03 20:30:07 +0800

    Update config

--- Client/DirzzleClient.ts
+++ Client/DirzzleClient.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "../Schema/DatabaseSchema.ts";
 
-const dbClient = drizzle(Deno.env.get("DB_URL"), { schema: { ...schema } });
+const dbClient = drizzle(Deno.env.get("DBURL"), { schema: { ...schema } });
 
 export default dbClient;
