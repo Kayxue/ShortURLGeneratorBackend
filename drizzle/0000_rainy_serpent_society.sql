12e49b9128efa34b8db2f87bbea02be86369a8bf
Kayxue <s0990006@gmail.com>
2024-12-19 14:46:29 +0800

    Re-initialize database

--- drizzle/0000_rainy_serpent_society.sql
+++ drizzle/0000_rainy_serpent_society.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS "shortUrl" (
-	"param" text PRIMARY KEY NOT NULL,
-	"url" text NOT NULL,
-	"userId" varchar(21),
-	"password" text,
-	"expireDate" timestamp
-);
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "shortUrlAnalytic" (
 	"param" text NOT NULL,
 	"country" text NOT NULL,
 	"count" integer NOT NULL,
 	CONSTRAINT "shortUrlAnalytic_param_country_pk" PRIMARY KEY("param","country")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "shortUrl" (
+	"param" text PRIMARY KEY NOT NULL,
+	"url" text NOT NULL,
+	"userId" varchar(21),
+	"password" text,
+	"expiredTime" timestamp
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user" (
@@ -22,13 +22,13 @@ CREATE TABLE IF NOT EXISTS "user" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "shortUrl" ADD CONSTRAINT "shortUrl_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;
+ ALTER TABLE "shortUrlAnalytic" ADD CONSTRAINT "shortUrlAnalytic_param_shortUrl_param_fk" FOREIGN KEY ("param") REFERENCES "public"."shortUrl"("param") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "shortUrlAnalytic" ADD CONSTRAINT "shortUrlAnalytic_param_shortUrl_param_fk" FOREIGN KEY ("param") REFERENCES "public"."shortUrl"("param") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "shortUrl" ADD CONSTRAINT "shortUrl_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- drizzle/0001_colossal_pestilence.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "shortUrl" RENAME COLUMN "expireDate" TO "expiredTime";
